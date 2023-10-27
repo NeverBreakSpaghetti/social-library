0dd5185db4642a533fea9b09300d4f498dfc540b
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2023-10-27 17:38:46 +0200

    refactor: clean test descriptions

--- test/insertBook.test.ts
+++ test/insertBook.test.ts
@@ -2,22 +2,22 @@ import request from 'supertest';
 
 const app = "http://localhost:3001";
 
-describe('POST /books to insert a book', () => {
-    it('should response with status code 201 when insert a book not in catalogue', async () => {
+describe('POST /books to insert a book not in catalogue', () => {
+    it('should response with status code 201', async () => {
         const newBook = {title: "Love story of a Gianni and his beer"};
 
         const response = await request(app).post("/books").send(newBook);
 
         expect(response.statusCode).toBe(201);
     });
-    it('should response with location header when insert a book not in catalogue', async () => {
+    it('should response with location header', async () => {
         const newBook = {title: "Love story of a Gianni and his beer"};
 
         const response = await request(app).post("/books").send(newBook);
 
         expect(response.header['location']).toBe('newBookLocation');
     });
-    it('should response with a body with book id when insert a book not in catalogue', async () => {
+    it('should response with a body with book id ', async () => {
         const newBook = {title: "Love story of a Gianni and his beer"};
 
         const response = await request(app).post("/books").send(newBook);
