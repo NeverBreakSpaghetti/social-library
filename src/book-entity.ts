a0ad7f25c3d783d41c0cad1f8dc09481b01a4e84
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2023-11-30 18:19:32 +0100

    refactor: use short syntax BookDto contractor

--- src/book-entity.ts
+++ src/book-entity.ts
@@ -1,12 +1,8 @@
 import {BookDto} from "./bookDto";
 
 export class BookEntity {
-    private constructor(private readonly id: string, private title: string, private author?: string, private pages?: number) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-    }
-    static create(id: string, book: Book) {
+    private constructor(private readonly id: string, private title: string, private author?: string, private pages?: number) {}
+    static create(id: string, book: BookDto) {
         return new BookEntity(id, book.title, book.author, book.pages);
     }
 }
