1496313e6c6f9f7a845b44999d160a5b1ea73a27
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2023-12-23 18:50:00 +0100

    refactor: update jest config to run only test under /test directory and only ts or tsx test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// https://jestjs.io/docs/en/configuration.html
+// https://basarat.gitbook.io/typescript/intro-1/jest#step-2-configure-jest
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-};
+    "roots": [
+        "<rootDir>/test"
+    ],
+    "testMatch": [
+        "**/__tests__/**/*.+(ts|tsx)",
+        "**/?(*.)+(spec|test).+(ts|tsx)"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+}
